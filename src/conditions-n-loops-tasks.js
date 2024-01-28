0b88f69ce3cde92bd4f27758c871dbdd892e6f0d
Pavel <verbipo@gmail.com>
2024-01-28 16:49:41 +0100

    feat: getNearestBigger

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -475,8 +475,30 @@ function shuffleChar(str, iterations) {
  * @returns {number} The nearest larger number, or original number if none exists.
  */
 
-function getNearestBigger(/* number */) {
-  throw new Error('Not implemented');
+function getNearestBigger(number) {
+  const digits = Array.from(String(number), Number);
+
+  let i = digits.length - 2;
+  while (i >= 0 && digits[i] >= digits[i + 1]) {
+    i -= 1;
+  }
+
+  if (i === -1) {
+    return number;
+  }
+
+  let j = digits.length - 1;
+  while (digits[j] <= digits[i]) {
+    j -= 1;
+  }
+
+  [digits[i], digits[j]] = [digits[j], digits[i]];
+  digits
+    .splice(i + 1)
+    .sort((a, b) => a - b)
+    .forEach((value) => digits.push(value));
+
+  return parseInt(digits.join(''), 10);
 }
 
 module.exports = {
